6bbd097861f8ed4ba9082158d516a44347f77712
carlos arellano <carlos@carloss-macbook-pro-3.local>
2018-10-29 18:43:15 -0400

    Initial Commit

---
+++ AgriculturalAppTests/AgriculturalAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  AgriculturalAppTests.swift
+//  AgriculturalAppTests
+//
+//  Created by carlos arellano on 10/29/18.
+//  Copyright © 2018 nyu.edu. All rights reserved.
+//
+
+import XCTest
+@testable import AgriculturalApp
+
+class AgriculturalAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
