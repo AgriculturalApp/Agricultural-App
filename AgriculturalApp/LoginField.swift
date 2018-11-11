cf408afb34981afd985bdf571999edda7fb51834
carlos arellano <carlos@carloss-macbook-pro-3.local>
2018-11-10 23:30:03 -0500

    Added some visuals and frontend graphics

---
+++ AgriculturalApp/LoginField.swift
@@ -0,0 +1,32 @@
+//
+//  LoginField.swift
+//  AgriculturalApp
+//
+//  Created by carlos arellano on 11/10/18.
+//  Copyright © 2018 nyu.edu. All rights reserved.
+//
+
+import UIKit
+
+class LoginField: UITextField {
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        underlineView.backgroundColor = UIColor.lightGray
+        
+        
+    }
+    
+    
+    
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
